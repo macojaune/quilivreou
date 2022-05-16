6fac263ba0637d41b8a2a1f3f6622200f4e05096
MarvinL.com <contact@marvinl.com>
2022-05-15 22:15:03 -0400

    :mag: (front) MetaTags

--- pages/index.js
+++ pages/index.js
@@ -7,9 +7,27 @@ export default function Home({sites}) {
 	return (
 		<div>
 			<Head>
-				<title>QuiLivreOù?</title>
-				<meta name="description"
-				      content="L&apos;annuaire des sites et boutiques qui livrent vers les Antilles"/>
+				//Primary Meta Tags
+				<title>QuiLivreOù? - L'annuaire des boutiques qui livrent aux Antilles-Guyane</title>
+				<meta name="title" content="QuiLivreOù? - L'annuaire des boutiques qui livrent aux Antilles-Guyane">
+				<meta name="description" content="Participe au développement du projet !
+				Trouve les sites qui livrent vers les Antilles-Guyane et partage tes sites marchands préférés.">
+
+				//  Open Graph / Facebook
+					<meta property="og:type" content="website">
+				<meta property="og:url" content="https://quilivreou.fr/">
+				<meta property="og:title" content="QuiLivreOù? - L'annuaire des boutiques qui livrent aux Antilles-Guyane">
+				<meta property="og:description" content="Participe au développement du projet !
+				Trouve les sites qui livrent vers les Antilles-Guyane et partage tes sites marchands préférés.">
+				<meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
+
+				//  Twitter
+				<meta property="twitter:card" content="summary_large_image">
+				<meta property="twitter:url" content="https://quilivreou.fr/">
+				<meta property="twitter:title" content="QuiLivreOù? - L'annuaire des boutiques qui livrent aux Antilles-Guyane">
+				<meta property="twitter:description" content="Participe au développement du projet !
+				Trouve les sites qui livrent vers les Antilles-Guyane et partage tes sites marchands préférés.">
+				<meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
 				<link rel="icon" href="/favicon.ico"/>
 			</Head>
 
