9bfe64bd949f8779958514f96e83affaa660b78e
MarvinL.com <contact@marvinl.com>
2022-05-15 21:56:56 -0400

    :rotating_light: (front) fix lint errors

--- pages/index.js
+++ pages/index.js
@@ -9,15 +9,15 @@ export default function Home({sites}) {
 			<Head>
 				<title>QuiLivreOù?</title>
 				<meta name="description"
-				      content="L'annuaire des sites et boutiques qui livrent vers les Antilles"/>
+				      content="L&apos;annuaire des sites et boutiques qui livrent vers les Antilles"/>
 				<link rel="icon" href="/favicon.ico"/>
 			</Head>
 
 			<main className="md:max-w-6xl mx-auto ">
 				<div className="text-center mb-3 px-4">
-					<a className="text-xs font-bold underline"
+					<a className="text-xs font-bold underline hover:decoration-4"
 					   href="https://t.me/macojaune">
-						On dirait que ce site à besoin d'un design ? T'as ça en stock
+						On dirait que ce site à besoin d&apos;un design ? T&apos;as ça en stock
 						??</a>
 				</div>
 				<div
@@ -39,21 +39,21 @@ export default function Home({sites}) {
 						En 2015, sur un coup de tête comme celui-ci, je lance quilivreou.fr. Un
 						thème wordpress gratuit avec un formulaire sur mesure et un fonctionnement
 						sommaire, simple. Efficace ? Pas trop.<br/>
-						Depuis rien n'a bougé <i>(les priorités, tu connais)</i>. Pourtant, si tu
+						Depuis rien n&apos;a bougé <i>(les priorités, tu connais)</i>. Pourtant, si tu
 						cherches <i>"qui livre en guadeloupe"</i>, le site est parmi les 1ers
 						résultats Google, <i className="font-semibold">"WouaaW"</i>.</p>
 				</section>
 				<section className="mt-5 px-4">
-					<h2 className="text-xl md:text-2xl font-semibold mb-3">Aujourd'hui j'ai eu
-						l'idée de le retaper d'<a className="underline"
+					<h2 className="text-xl md:text-2xl font-semibold mb-3">Aujourd&apos;hui j&apos;ai eu
+						l&apos;idée de le retaper d&apos;<a className="underline hover:decoration-4"
 						                          href="https://www.figma.com/file/QCZURRHmMDw27kxeLo0px1/QuiLivreO%C3%B9-Road-to-v2"
-						                          target="_blank">une
+						                          target="_blank" rel="noreferrer">une
 							façon stimulante</a>.</h2>
 					<div className="flex justify-center">
 						<ol className="list-decimal text-lg">
 							<li>Je développe et publie une fonctionnalité</li>
 							<li className="font-semibold text-lg">Vous testez et faites péter
-								l'objectif
+								l&apos;objectif
 							</li>
 							<li>Je passe à la suivante et <b>RE-BE-LOTE</b></li>
 						</ol>
@@ -69,26 +69,26 @@ export default function Home({sites}) {
 					</div>
 					<div className="sticky bottom-0 inset-x-0">
 						<ShareLink
-							text="C'est parti pour la refonte @quilivreou il nous faut 33 tweets pour atteindre la prochaine étape !"
+							text="C&apos;est parti pour la refonte @quilivreou il nous faut 33 tweets pour atteindre la prochaine étape !"
 							link="https://quilivreou.fr" hashtags={['BidimBo']}>
 							{link => <a
 								className="inline-block p-6 w-full h-full text-center text-2xl bg-black text-white"
-								href={link} target="_blank">Partage le projet</a>}
+								href={link} target="_blank"  rel="noreferrer">Partage le projet</a>}
 						</ShareLink>
 					</div>
 				</section>
 				<section className="mt-5 px-4">
 					<h2 className="text-xl md:text-2xl font-bold mb-3">Les sites{" "}
 						<small className="font-normal italic text-sm md:text-base">ajoutés sur
-							l'ancienne version</small></h2>
+							l&apos;ancienne version</small></h2>
 					<div className="flex flex-row flex-wrap gap-2">
-						{sites && sites.map(s => <a className="underline font-semibold"
+						{sites && sites.map(s => <a className="underline hover:decoration-4 font-semibold"
 						                            key={s?.id}>{s?.title?.rendered}</a>)}
 					</div>
 				</section>
 			</main>
 			<footer className="px-6 my-12 text-center">
-				<p className="font-semibold text-blue-500">Pondu avec humour par <a className="underline" href="https://marvinl.com" target="_blank">MarvinL.com</a></p>
+				<p className="font-semibold text-blue-500">Pondu avec humour par <a className="underline hover:decoration-4" href="https://marvinl.com" target="_blank" rel="noreferrer">MarvinL.com</a></p>
 			</footer>
 		</div>
 	)
