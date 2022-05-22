02e1f23b974fab8cf0f5c0aff7c0dc616f3a3628
MarvinL.com <contact@marvinl.com>
2022-05-22 19:34:04 -0400

    remove token from code

--- pages/api/twitter.js
+++ pages/api/twitter.js
@@ -1,9 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-	fetch("https://api.twitter.com/2/tweets/search/recent?query=%23BidimBo%20%40quilivreou&since_id=1526043100698890240", {headers: {Authorization: "Bearer AAAAAAAAAAAAAAAAAAAAAIOIcwEAAAAABL2ZFqeILIbMjcLodAVh61H1DNc%3DNgHophryvzSB7zZkghepWQm7RvD00TxYXDLQhqbtyIJDXnYj0v"}}).then(res =>
+	fetch("https://api.twitter.com/2/tweets/search/recent?query=%23BidimBo%20%40quilivreou&since_id=1526043100698890240", {headers: {Authorization: "Bearer " + process.env.NEXT_PUBLIC_TWITTER_TOKEN}}).then(res =>
 		res.json())
-		.then(json => res.status(200).send(json?.meta?.result_count))
+		.then(json => res.status(200).send(json?.meta?.result_count)).catch(e =>
 
-	//res.status(200).json(data)
+		//error
+		res.status(404).json(e))
 }
